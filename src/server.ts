d6cd8d6a0355aa3fe9c905ab831f7f4bafb414bd
Kayo Thyerre <kayothyerrework@gmail.com>
2024-10-09 13:10:13 -0300

    configuracoes express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-const teste = 'sadlmasçd'
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/movies", (req, res) => {
+    res.send("Lista de filmes");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta http://localhost:${port}`);
+});
