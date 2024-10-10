6168839cc42f8f71e5f02d465de67c8b53e12f0f
Kayo Thyerre <kayothyerrework@gmail.com>
2024-10-10 15:26:55 -0300

    esquema dos módulos no BD pelo arquivo .prisma

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
-app.get("/movies", (req, res) => {
-    res.send("Lista de filmes");
+app.get("/movies", async (req, res) => {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 });
 
 app.listen(port, () => {
